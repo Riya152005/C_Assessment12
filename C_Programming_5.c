85f011c1a3c6ae7ab8b12abb37f81650eacb68fd
Riya152005 <riyarathna1507@gmail.com>
2026-07-01 23:20:11 +0530

    Create C_Programming_5.c

---
+++ C_Programming_5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    long long num1, num2;
+    printf("Enter first number: ");
+    scanf("%lld", &num1);
+    printf("Enter second number: ");
+    scanf("%lld", &num2);
+    printf("Result = %lld", num1 * num2);
+}
